634633db87ae5670b1a5bb0f5a02696b77233c76
Ramkumar-2269 <ramkumarakula84@gmail.com>
2024-01-23 17:50:51 +0530

    added some changes

--- src/SalesforceSwitch.js
+++ src/SalesforceSwitch.js
@@ -142,12 +142,17 @@ const SalesforceSwitch = () => {
   }
 
   const handleLogout = () => {    
-    window.location.href=`${instanceUrl}/secur/logout.jsp`;
-    setAccessToken(null);
-    setInstanceUrl(null);
-    localStorage.removeItem(accessToken);
-    localStorage.removeItem(instanceUrl);
-    window.location.href = `${redirectUri}`
+    var salesforceLogoutUrl = 'https://login.salesforce.com/services/auth/sso/logout';
+
+    // Redirect URI (should match the one configured in your connected app)
+    var redirectUri = 'https://thunderous-dolphin-396c8b.netlify.app';
+
+    // Build the logout URL with the logout parameter and redirect URI
+    var logoutUrl = salesforceLogoutUrl + '?logout=true&redirect_uri=' + encodeURIComponent(redirectUri);
+
+    // Redirect the user to the logout URL
+    window.location.href = logoutUrl;
+
   }
 
 
